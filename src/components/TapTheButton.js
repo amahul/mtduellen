a129cf5b94b26370e32733e6db5a232a48e8b54e
amahul <amandis1@live.se>
2020-12-02 16:34:00 +0100

    merged

--- src/components/TapTheButton.js
+++ src/components/TapTheButton.js
@@ -22,7 +22,7 @@ import avsluta from '../bilder/avsluta.png';
 import braJobbat from '../bilder/braJobbat.png';
 
 import spelregler from '../bilder/spelregler.png';
-import { BorderlessButton } from 'react-native-gesture-handler';
+import {BorderlessButton} from 'react-native-gesture-handler';
 
 const store = require('./Storage');
 
@@ -146,56 +146,46 @@ const TapTheButton = ({}) => {
           link={true}
           action="/"
         /> */}
-      
 
-        <Modal
-          style={styles.modal}
-          backdrop={false}
-          position={'center'}
-          isOpen={secondModal}>
-            <Image
-              source={braJobbat}
-              style={{ resizeMode: 'contain',
-              width: 200,
-              alignItems: 'flex-start',
-              height: 70,
-              bottom: 0,
-              margin: 5,
-              }} 
-              /> 
-          <Text
-            style={{
-              fontSize: 30,
-              fontWeight: 'bold',
-              top: 23,
-            }}>
-            {endText}
-          </Text>
+      <Modal
+        style={styles.modal}
+        backdrop={false}
+        position={'center'}
+        isOpen={secondModal}>
+        <Image
+          source={braJobbat}
+          style={{
+            resizeMode: 'contain',
+            width: 200,
+            alignItems: 'flex-start',
+            height: 70,
+            bottom: 0,
+            margin: 5,
+          }}
+        />
+        <Text
+          style={{
+            fontSize: 30,
+            fontWeight: 'bold',
+            top: 23,
+          }}>
+          Du fick {count} poäng!
+        </Text>
 
-          {/* <Link to="/" underlayColor="#f0f4f7">
+        {/* <Link to="/" underlayColor="#f0f4f7">
             <Text>Nästa spel</Text>
           </Link> */}
 
-          <Link to="/">
-            <Image
-              source={avsluta}
-              style={{width: 250, height: 70, bottom: 0, margin: 5, top: 55}}
-            />
-          </Link>
-        </Modal>
+        <Link to="/">
+          <Image
+            source={avsluta}
+            style={{width: 250, height: 70, bottom: 0, margin: 5, top: 55}}
+          />
+        </Link>
+      </Modal>
 
-        {/* )} */}
-        {/* LAUNCHER */}
-        {this.state.showLauncher && (
-          <Launcher
-            running={firstTimer}
-            startGame={startGame}
-          />)}
-
-      {/* )} */}
       {/* LAUNCHER */}
       {showLauncher && <Launcher running={firstTimer} startGame={startGame} />}
-      {/* <Button onPress={startGame} title="Kör igång" /> */}
     </View>
   );
 };
